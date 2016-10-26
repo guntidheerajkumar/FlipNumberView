57998eee3cca9878bcb25c10f5cd9268d4e81f8c
Dheeraj Kumar G <guntidheerajkumar@gmail.com>
2016-10-26 14:29:44 +0530

    FlipNumberView
    FlipNumberView generates random number and displays in flip view fashion

---
+++ FlipNumberView/ApiDefinition.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Foundation;
+using ObjCRuntime;
+using UIKit;
+
+namespace FlipNumberView
+{
+	// @interface JTNumberScrollAnimatedView : UIView
+	[BaseType(typeof(UIView))]
+	interface JTNumberScrollAnimatedView
+	{
+		// @property (nonatomic, strong) NSNumber * value;
+		[Export("value", ArgumentSemantic.Strong)]
+		NSNumber Value { get; set; }
+
+		// @property (nonatomic, strong) UIColor * textColor;
+		[Export("textColor", ArgumentSemantic.Strong)]
+		UIColor TextColor { get; set; }
+
+		// @property (nonatomic, strong) UIFont * font;
+		[Export("font", ArgumentSemantic.Strong)]
+		UIFont Font { get; set; }
+
+		// @property (assign, nonatomic) CFTimeInterval duration;
+		[Export("duration")]
+		double Duration { get; set; }
+
+		// @property (assign, nonatomic) CFTimeInterval durationOffset;
+		[Export("durationOffset")]
+		double DurationOffset { get; set; }
+
+		// @property (assign, nonatomic) NSUInteger density;
+		[Export("density")]
+		nuint Density { get; set; }
+
+		// @property (assign, nonatomic) NSUInteger minLength;
+		[Export("minLength")]
+		nuint MinLength { get; set; }
+
+		// @property (assign, nonatomic) BOOL isAscending;
+		[Export("isAscending")]
+		bool IsAscending { get; set; }
+
+		// -(void)startAnimation;
+		[Export("startAnimation")]
+		void StartAnimation();
+
+		// -(void)stopAnimation;
+		[Export("stopAnimation")]
+		void StopAnimation();
+	}
+}
---
+++ SampleApplication/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FlipNumberView;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace SampleApplication
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			var flipView = new JTNumberScrollAnimatedView();
+			flipView.TextColor = UIColor.Red;
+			flipView.Duration = 1.0f;
+			flipView.MinLength = 4;
+			flipView.Font = UIFont.SystemFontOfSize(20);
+			flipView.Value = NSNumber.FromDouble(259);
+			flipView.Frame = new CGRect(10, 60, this.View.Frame.Width - 20, 100);
+			flipView.StartAnimation();
+			this.View.AddSubview(flipView);
+		}
+	}
+}
